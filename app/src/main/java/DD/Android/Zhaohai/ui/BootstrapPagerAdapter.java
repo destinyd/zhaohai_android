90d159e4b557e3180bc3d2599215cc3333612a5d
destinyd <destinyd_war@163.com>
2013-01-07 19:37:49 +0800

    zhaohai android with bootstrap

---
+++ app/src/main/java/DD/Android/Zhaohai/ui/BootstrapPagerAdapter.java
@@ -0,0 +1,70 @@
+
+
+package DD.Android.Zhaohai.ui;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import DD.Android.Zhaohai.R;
+
+/**
+ * Pager adapter
+ */
+public class BootstrapPagerAdapter extends FragmentPagerAdapter {
+
+    private final Resources resources;
+
+    /**
+     * Create pager adapter
+     *
+     * @param resources
+     * @param fragmentManager
+     */
+    public BootstrapPagerAdapter(Resources resources, FragmentManager fragmentManager) {
+        super(fragmentManager);
+        this.resources = resources;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Bundle bundle = new Bundle();
+        switch (position) {
+        case 0:
+            NewsListFragment newsFragment = new NewsListFragment();
+            newsFragment.setArguments(bundle);
+            return newsFragment;
+        case 1:
+            UserListFragment userListFragment = new UserListFragment();
+            userListFragment.setArguments(bundle);
+            return userListFragment;
+        case 2:
+            CheckInsListFragment checkInsFragment = new CheckInsListFragment();
+            checkInsFragment.setArguments(bundle);
+            return checkInsFragment;
+        default:
+            return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+        case 0:
+            return resources.getString(R.string.page_friends);
+        case 1:
+            return resources.getString(R.string.page_users);
+        case 2:
+            return resources.getString(R.string.page_activities);
+        default:
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/DD/Android/Zhaohai/ui/NewsActivity.java
@@ -0,0 +1,44 @@
+package DD.Android.Zhaohai.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.actionbarsherlock.view.MenuItem;
+import DD.Android.Zhaohai.BootstrapServiceProvider;
+import DD.Android.Zhaohai.core.News;
+import DD.Android.Zhaohai.R;
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
+import com.google.inject.Inject;
+
+import roboguice.inject.InjectExtra;
+import roboguice.inject.InjectView;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
+import static DD.Android.Zhaohai.core.Constants.Extra.NEWS_ITEM;
+
+public class NewsActivity extends BootstrapActivity {
+
+    @InjectExtra(NEWS_ITEM) protected News newsItem;
+
+    @InjectView(R.id.tv_title) protected TextView title;
+    @InjectView(R.id.tv_content) protected TextView content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.news);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        setTitle(newsItem.getTitle());
+
+        title.setText(newsItem.getTitle());
+        content.setText(newsItem.getContent());
+
+    }
+
+}
